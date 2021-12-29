0426ae0a0dcda0f2cc1d75337291a502e481b5d2
Martin Yanchev <myanch200@gmail.com>
2021-12-29 16:23:59 +0000

    update server

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var host;
 var handleRequest = function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
-  response.write("Martin Yanchev DevOps Coursework 2! | Running on: ");
+  response.write("DevOps Coursework 2! | Running on: ");
   response.write(host);
-  response.end(" | v=1\n");
+  response.end(" | v=0\n");
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
 
 var www = http.createServer(handleRequest);
